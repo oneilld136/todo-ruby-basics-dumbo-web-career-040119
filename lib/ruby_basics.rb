7b03e211c0ed1d460a17d78bd091d13674ae9b3e
oneilld136 <oneilld136@gmail.com>
2019-03-19 16:45:03 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,26 @@
-def division(num1, num2)
- 
+def division(num1=12,num2=2)
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  value="Bob"
 end
 
-def argue
-  
+def argue (name)
+  argue=(name)
 end
 
-def greeting
-
+def greeting (hello,name)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(type="cheese")
+  type
 end
